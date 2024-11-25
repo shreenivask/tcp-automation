dd4d07700cdb35cf8b5cc8443e975f53adf87b82
samritak <152491485+samritak@users.noreply.github.com>
2024-11-25 18:11:27 +0530

    Delete user/controllers directory

--- user/controllers/test_controller.py
+++
@@ -1,9 +0,0 @@
-from user.models.test import Test
-
-
-def get_all_tests():
-    return Test.query.order_by(Test.id.desc()).limit(10)
-
-def get_aarp_tests():
-    aarp_tests = Test.query.filter(Test.test_ticket.like('AARP%')).order_by(Test.id.desc()).limit(10)
-    return aarp_tests
