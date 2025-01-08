522d152d8f2e68c27660756411b7d965d6856f65
samritak <152491485+samritak@users.noreply.github.com>
2025-01-08 15:47:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_secret_key'
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/tc_python_framework'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'postgresql://root:ZSYIcH1QS1n19PU5Y3J3JwcRQgJwQuhE@dpg-ctv1uhtds78s738m7leg-a.oregon-postgres.render.com:5432/qap_framework_database_ieai?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = "postgresql://root:ZSYIcH1QS1n19PU5Y3J3JwcRQgJwQuhE@dpg-ctv1uhtds78s738m7leg-a.oregon-postgres.render.com:5432/qap_framework_database_ieai?sslmode=require"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PASSWORD_HASH_SALT = "tc#123456"
